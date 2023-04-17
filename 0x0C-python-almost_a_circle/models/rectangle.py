f1cea0c4f7977ab31ab97b3ad26b8dc92d3099f2
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2023-04-17 05:56:30 -0700

    7. Display #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,6 +79,19 @@ class Rectangle(Base):
             self.id, self.x, self.y, self.width, self.height)
 
     def area(self):
-        """The area of the rectangle(Update)"""
+        """Update-The area of the rectangle"""
         return self.width * self.height
+
+    def update(self, *args):
+        """Update-the public method that asssigns an argument to each attribute"""
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
 
