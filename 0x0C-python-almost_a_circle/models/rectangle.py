352643a4009776094c7f8dc182cb5ad3875b0941
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2023-04-17 07:08:40 -0700

    9. Update #1

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,16 +82,48 @@ class Rectangle(Base):
         """Update-The area of the rectangle"""
         return self.width * self.height
 
-    def update(self, *args):
-        """Update-the public method that asssigns an argument to each attribute"""
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.width = args[1]
-        if len(args) >= 3:
-            self.height = args[2]
-        if len(args) >= 4:
-            self.x = args[3]
-        if len(args) >= 5:
-            self.y = args[4]
+#    def update(self, *args):
+#        """Update-the public method that asssigns an argument to each attribute"""
+#        if len(args) >= 1:
+#            self.id = args[0]
+#        if len(args) >= 2:
+#            self.width = args[1]
+#        if len(args) >= 3:
+#            self.height = args[2]
+#        if len(args) >= 4:
+#            self.x = args[3]
+#        if len(args) >= 5:
+#            self.y = args[4]
+
+    def update(self, *args, **kwargs):
+        """Update: changing the prototype that assigns a key that hasvalue argument to attributes"""
+        if args:
+            for i in range(len(args)):
+                """Intializing the count for non keyword arguments"""
+                if i == 0:
+                    if type(args[i]) is not int:
+                        raise TypeError("id must be an integer")
+                    self.id = args[i]
+                elif i == 1:
+                    self.width = args[i]
+                elif i == 2:
+                    self.height = args[i]
+                elif i == 3:
+                    self.x = args[i]
+                elif i == 4:
+                    self.y = args[i]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if type(value) is not int:
+                        raise TypeError("id must be an integer")
+                    self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
 
