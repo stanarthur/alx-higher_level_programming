7a34d3489ea3e66a69229ec319f6bfc53e73e16e
Stanhope Chinonso <stanhope.c.arthur@gmail.com>
2023-04-17 07:39:56 -0700

    11. Square size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,7 +4,22 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Defining square class"""
+    """Defining the square class"""
     def __init__(self, size, x=0, y=0, id=None):
-        """Initializing square class"""
+        """Initializing the square class"""
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Printing the square class by overloading __str__ method"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
+    @property
+    def size(self):
+        """Getting the size of square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setting the size of square"""
+        self.width = value
+        self.height = value
